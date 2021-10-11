ea570af34c169250d65bd9627ac4053498728edd
Евгений <maleevea@gmail.com>
2021-10-11 15:17:02 +0500

    get multiple drivers

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -23,7 +23,26 @@ $assetDir = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/admi
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body class="hold-transition sidebar-mini">
+<!-- Modal -->
+<div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div><body class="hold-transition sidebar-mini">
 <?php $this->beginBody() ?>
 
 <div class="wrapper">
--- backend/web/js/searchDriver.js
+++ backend/web/js/searchDriver.js
@@ -24,6 +24,37 @@ $("#searchDriver").on("click", 'button.btn', function(){
 
             sleep(1000);
 
+            console.log(msg.driver_profiles);
+            if(msg.driver_profiles.length > 1 ){
+                let html='<table class="table">\n' +
+                    '  <thead class="thead-light">\n' +
+                    '    <tr>\n' +
+                    '      <th scope="col">#</th>\n' +
+                    '      <th scope="col">Имя</th>\n' +
+                    '      <th scope="col">Фамилия</th>\n' +
+                    '      <th scope="col">Статус</th>\n' +
+                    '      <th scope="col">Комментарий</th>\n' +
+                    '      <th scope="col"></th>\n' +
+                    '    </tr>\n' +
+                    '  </thead>\n' +
+                    '  <tbody>';
+                msg.driver_profiles.forEach(function(item, i, arr) {
+
+                    html += '<tr>\n' +
+                    '      <th scope="row">'+i+'</th>\n' +
+                    '      <td>'+arr[i].driver_profile.first_name+'</td>\n' +
+                    '      <td>'+arr[i].driver_profile.last_name+'</td>\n' +
+                    '      <td>'+arr[i].driver_profile.work_status+'</td>\n' +
+                    '      <td>'+arr[i].driver_profile.comment+'</td>\n' +
+                    '      <td><button class="btn btn-primary getDriver">Выбрать</button></td>\n' +
+                    '    </tr>';
+                });
+                html += '</tbody>\n' +
+                    '</table>';
+                $('.modal-body').html(html)
+
+                $('#exampleModalCenter').modal('show');
+            }
             $("#driver-first_name").val(msg.driver_profiles[0].driver_profile.first_name);
             $("#driver-last_name").val(msg.driver_profiles[0].driver_profile.last_name);
             $("#driver-yandex_id").val(msg.driver_profiles[0].driver_profile.id);
